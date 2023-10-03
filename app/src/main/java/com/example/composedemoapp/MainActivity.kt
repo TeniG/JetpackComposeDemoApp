a36acb4ebeaa8af099177ef34b57e3cf49e033bc
TeniG <tenigada@gmail.com>
2023-10-03 18:24:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/composedemoapp/MainActivity.kt
@@ -0,0 +1,15 @@
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composedemoapp.ui.theme.ComposeDemoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
